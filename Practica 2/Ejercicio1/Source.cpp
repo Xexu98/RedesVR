779014e55c49171641a2d673f197784fc10ed1c6
Xexu98 <jesusf05@ucm.es>
2020-04-23 17:24:10 +0200

    Creada carpeta ejercicio 2 termiando el 1

--- Practica 2/Ejercicio1/Source.cpp
+++ Practica 2/Ejercicio1/Source.cpp
@@ -25,33 +25,33 @@ host (numeric)
 ./addrinfo 127.0.0.1 80
 ./addrinfo www.ucm.es http
 */
-int main(int argc, char **argv)
-{
-struct addrinfo hints;
-struct addrinfo * res;
+int main(int argc, char** argv){
 
-memset(&hints, 0, sizeof(struct addrinfo));
+    struct addrinfo hints;
+    struct addrinfo *res;
+    
+    memset((void*)&hints, 0, sizeof(struct addrinfo));
 
-hints.ai_family = AF_INET;
-hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_family = AF_UNSPEC;
+   
 
-int rc = getaddrinfo(argv[1], argv[2], &hints, &res);
+    int rc = getaddrinfo(argv[1], argv[2], &hints, &res);
 
-if ( rc != 0 )
-{
-std::cerr << "getaddrinfo: " << gai_strerror(rc) << std::endl;
-return -1;
-}
+    if(rc != 0){
+        std::cout << "Error getaddrinfo: " << gai_strerror(rc) << std::endl;
+        return -1;
+    }
 
-char host[NI_MAXHOST];
-char service[NI_MAXSERV];
+    for(struct addrinfo * resaux=res; resaux!=0;resaux = resaux->ai_next){
+        char host[NI_MAXHOST];
+        char serv[NI_MAXSERV];
 
-getnameinfo(res->ai_addr, res->ai_addrlen, host, NI_MAXHOST, service,
-NI_MAXSERV, NI_NUMERICHOST | NI_NUMERICSERV);
+        getnameinfo(resaux->ai_addr, resaux->ai_addrlen, host, NI_MAXHOST, serv, NI_MAXSERV, 
+        NI_NUMERICHOST | NI_NUMERICSERV);
 
-std::cout << "IP: " << host << " PUERTO: " << service << std::endl;
+        std::cout << "Host: " << host << "\tPuerto: " << serv <<"\n";
+    }
+    freeaddrinfo(res);
 
-freeaddrinfo(res);
-
-return 0;
+    return 0;
 }
